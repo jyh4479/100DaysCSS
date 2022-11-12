02c887e0be0c852236e69c564d4b8a3c49dced62
jyh4479 <jyh.dev@gmail.com>
2022-11-12 22:58:34 +0900

    UPLOAD 2 DAY

---
+++ 2/index.js
@@ -0,0 +1,13 @@
+const lineBox = window.document.querySelector(".line-box");
+const lines = window.document.querySelectorAll(".line-box > div");
+
+lineBox.addEventListener("click", () => {
+    lines.forEach(line => {
+        line.classList.toggle("active");
+    });
+})
+
+// $('.menu-icon').bind('click', function() {
+//     $(this).toggleClass('active');
+//     $(this).find('div').removeClass('no-animation');
+// });
